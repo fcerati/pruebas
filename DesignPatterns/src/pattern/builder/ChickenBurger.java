631c0b20a16efe3240b8ca89b71f5d7f5beedf48
Franco Cerati <ceratifranco@gmail.com>
2015-05-21 13:52:48 -0300

    Builder Pattern - Creational Pattern

---
+++ DesignPatterns/src/pattern/builder/ChickenBurger.java
@@ -0,0 +1,15 @@
+package pattern.builder;
+
+public class ChickenBurger extends Burguer {
+
+	@Override
+	public String name() {
+		return "Chicken Burger";
+	}
+
+	@Override
+	public float price() {
+		return 50.5f;
+	}
+
+}
---
+++ DesignPatterns/src/pattern/builder/VegBurger.java
@@ -0,0 +1,16 @@
+package pattern.builder;
+
+public class VegBurger extends Burguer {
+
+	@Override
+	public String name() {
+		return "Veg Burger";
+
+	}
+
+	@Override
+	public float price() {
+		return 25.0f;
+	}
+
+}
